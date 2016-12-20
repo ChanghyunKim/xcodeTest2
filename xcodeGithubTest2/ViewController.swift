51dcfdfe10f53794453410322d196f7fcbdba9d9
김창현 <kbspkpk@naver.com>
2016-12-20 10:44:27 +0900

    Initial Commit

---
+++ xcodeGithubTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xcodeGithubTest2
+//
+//  Created by 김창현 on 2016. 12. 20..
+//  Copyright © 2016년 Sero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
